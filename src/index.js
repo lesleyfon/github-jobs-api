aeb67473773f081170aafeb103c77a51b5b69a77
lesley <lesleybanadzem2017@gmail.com>
2020-10-18 14:27:54 -0700

    ensure that styles were working on breakpoints

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const Header = require("./main/Header.js");
+const Main = require("./main/Main.js");
 
 // RUn function on page load
 (function () {
 	const header = new Header();
 	header.buildHeader();
+	const main = new Main();
+	main.renderMain();
 })();
 
 // if (process.env.NODE_ENV === "development") {
---
+++ src/main/Main.js
@@ -0,0 +1,7 @@
+class Main {
+	renderMain() {
+		const mainBody = document.getElementById("container");
+	}
+}
+
+module.exports = Main;

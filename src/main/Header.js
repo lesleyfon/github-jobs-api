06a4936e0759c3950286d699000a2ffeba14faa7
lesley <lesleybanadzem2017@gmail.com>
2020-10-18 13:40:05 -0700

    added styles for the header

--- src/main/Header.js
+++ src/main/Header.js
@@ -1,9 +1,38 @@
 class Header {
-	constructor() {
-		// this.mobile_svg_url = require("./../assets/mobile/bg-pattern-header.svg");
+	constructor() {}
+
+	addLogo() {
+		const logoDiv = document.createElement("div");
+		logoDiv.setAttribute("id", "id_logo");
+		return logoDiv;
 	}
 	buildHeader() {
 		const header = document.getElementById("id_header");
+
+		const logoThemeContainer = document.createElement("div");
+		logoThemeContainer.setAttribute("id", "logo-theme-container");
+
+		logoThemeContainer.appendChild(this.addLogo());
+		logoThemeContainer.appendChild(this.themeContainer());
+
+		header.appendChild(logoThemeContainer);
+	}
+
+	themeContainer() {
+		const themeContainer = document.createElement("div");
+		themeContainer.className = "theme-container";
+		const themeLabel = `
+				<div id="lightMode"></div>
+				<label class="switch">
+					<input type="checkbox" checked>
+					<span class="slider round"></span>
+				</label>
+				<div id="darkMode"></div>
+		`;
+
+		themeContainer.innerHTML = themeLabel;
+
+		return themeContainer;
 	}
 }
 module.exports = Header;
